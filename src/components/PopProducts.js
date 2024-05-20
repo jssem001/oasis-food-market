53d4bb23f594c6380b18b14b79b85006eba0828b
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-20 15:22:32 +0300

    slider 2

--- src/components/PopProducts.js
+++ src/components/PopProducts.js
@@ -6,18 +6,45 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+  
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "green" }}
+        onClick={onClick}
+      />
+    );
+  }
+
 function PopularProducts(){
     const products = useContext(ProductsContext);
     var settings = {
         dots: true,
         infinite: true,
-        speed: 250,
+        speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        cssEase: "linear",
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
       };
 
     return(
-        <Card className="ml-3 mr-3 mb-3 max-w-7xl bg-green-500 border-2 border-green-500">
+        <Card className="ml-3 mr-3 mb-3 max-w-7xl border-2 border-green-500">
             <h5 className="ml-2 mb-2 text-2xl font-bold tracking-tight">
             Popular Products and Services
             </h5>
