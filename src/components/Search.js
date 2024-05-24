1d4afb7761b9d16e40ad6ead4214e996e1981077
Joshua Ssemwanga <jssem001@plattsburgh.edu>
2024-05-24 15:12:52 +0300

    added search

---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Search() {
+  
+
+//   function handleSearchChange(event) {
+//     onSearchChange(event.target.value);
+//   }
+
+  return (
+    <div className="ui large fluid icon input flex items-center max-w-lg mx-auto">
+      <input
+        className="rounded-md w-full"
+        type="text"
+        placeholder="Search Items..."
+        // value={search} 
+        // onChange={handleSearchChange}
+      />
+      <i className="circular search link icon" ></i>
+    </div>
+  );
+}
+
+export default Search;
--- src/pages/Latest.js
+++ src/pages/Latest.js
@@ -1,5 +1,6 @@
 import React from "react"; import Navbar from "../components/Navbar";
 import Footer from "../components/Footer"; import ProductList from "../components/ProductList";
+import Search from "../components/Search";
 
 
 function Latest(){
@@ -8,6 +9,7 @@ function Latest(){
         <div>
             <Navbar />
             <h1 className="ml-3 mb-3 text-2xl text-center font-bold">All Products</h1>
+            <Search />
             <ProductList />
             <Footer />
         </div>
